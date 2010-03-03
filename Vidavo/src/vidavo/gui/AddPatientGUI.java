bcfee83315f44a3a2c42b130419bc4e32cd9c7c8
kreonerebus <kreonerebus@6ec47230-67fa-67b0-c59e-b28e42bb8b13>
2010-03-03 16:02:00 +0000

    fixed typos

--- Vidavo/src/vidavo/gui/AddPatientGUI.java
+++ Vidavo/src/vidavo/gui/AddPatientGUI.java
@@ -73,7 +73,7 @@ public class AddPatientGUI extends JFrame implements ActionListener{
         chronicDiseasesPane = new ChronicDiseasesPane(resourceMap);
         contactsPane = new ContactsPane(resourceMap);
         photosPane = new PhotosPane(resourceMap);
-        personalInfoPane = new JPanel();
+        personalInfoPane = new PersonalInfoPane(pm);
         saveButton = new JButton();
         cancelButton = new JButton();
 
