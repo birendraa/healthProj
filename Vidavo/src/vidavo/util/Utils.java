a64e1937e2cd21854f040a7858722e2b36a1f9ef
master2004 <master2004@6ec47230-67fa-67b0-c59e-b28e42bb8b13>
2010-03-11 07:00:55 +0000

    working on photos pane

--- Vidavo/src/vidavo/util/Utils.java
+++ Vidavo/src/vidavo/util/Utils.java
@@ -26,14 +26,4 @@ public class Utils {
         return ext;
     }
 
-    /** Returns an ImageIcon, or null if the path was invalid. */
-    protected static ImageIcon createImageIcon(String path) {
-        java.net.URL imgURL = Utils.class.getResource(path);
-        if (imgURL != null) {
-            return new ImageIcon(imgURL);
-        } else {
-            System.err.println("Couldn't find file: " + path);
-            return null;
-        }
-    }
 }
